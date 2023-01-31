39356d1e615e8b6acc955cfd3e416b036f5950d2
TVmall <ariukariuka>
2023-01-31 09:10:10 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
       <>
          <Button variant="blue" textElement='All' imgElement={img} />
-         <Button variant="black" textElement='Txt' imgElement={img} />
+         <Button variant="blue" textElement='Txt' imgElement={img} />
          <Button variant="black" textElement='Allfdasf' imgElement={img} />
          <Button variant="black" textElement='Allfdasf' imgElement={img} />
       </>
